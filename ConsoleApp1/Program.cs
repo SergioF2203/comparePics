e48dbdbd123d4c724a5d8a600b90f4e5bd5e7175
SergioF <fs220381@gmail.com>
2019-08-17 12:19:20 +0300

    first commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            btmpSource = new Bitmap();
+            Bitmap bitmap = new Bitmap(btmpSource);
         }
     }
 }
