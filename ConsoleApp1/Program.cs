b1919d89629a34ca01c98cc80899b2ec6790b1b7
SergioF <fs220381@gmail.com>
2019-08-17 17:51:29 +0300

    first pic compare with all pics

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,7 +12,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string filePathName = @"D:\C#\testpic.png";
-            int stepPic = 140;
+            int stepPicX = 140;
+            int stepPicY = 140;
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
             Bitmap picfile = new Bitmap(filePathName);
@@ -22,41 +23,45 @@ namespace ConsoleApp1
             int allcount = 0;
             //int step = 140;
             int countOfPic = 0;
-
-            for (int step = stepPic; step<bitmap.Width; )
+            for (int stepY = 0; stepY < bitmap.Height;)
             {
-                for (int i = 0; i < 140; i++)
+                for (int stepX = stepPicX; stepX < bitmap.Width;)
                 {
-                    for (int j = 0; j < 140; j++)
+                    for (int i = 0; i < 140; i++)
                     {
-                        if (bitmap.GetPixel(i, j).R == bitmap.GetPixel(i + step, j).R &&
-                            bitmap.GetPixel(i, j).G == bitmap.GetPixel(i + step, j).G &&
-                            bitmap.GetPixel(i, j).B == bitmap.GetPixel(i + step, j).B)
-                            count++;
-                        //else
-                        //{
-                        //    Console.WriteLine($"x = {i}, y = {j} ");
-                        //    Console.WriteLine($"R={bitmap.GetPixel(i, j).R}, " +
-                        //        $"G={bitmap.GetPixel(i, j).G}, B={bitmap.GetPixel(i, j).B}, A={bitmap.GetPixel(i, j).A}");
-                        //    Console.WriteLine($"R={bitmap.GetPixel(i + 280, j).R}, " +
-                        //        $"G={bitmap.GetPixel(i + 280, j).G}, B={bitmap.GetPixel(i + 280, j).B}, A={bitmap.GetPixel(i + 280, j).A}");
+                        for (int j = 0; j < 140; j++)
+                        {
+                            if (bitmap.GetPixel(i, j).R == bitmap.GetPixel(i + stepX, j + stepY).R &&
+                                bitmap.GetPixel(i, j).G == bitmap.GetPixel(i + stepX, j + stepY).G &&
+                                bitmap.GetPixel(i, j).B == bitmap.GetPixel(i + stepX, j + stepY).B)
+                                count++;
+                            //else
+                            //{
+                            //    Console.WriteLine($"x = {i}, y = {j} ");
+                            //    Console.WriteLine($"R={bitmap.GetPixel(i, j).R}, " +
+                            //        $"G={bitmap.GetPixel(i, j).G}, B={bitmap.GetPixel(i, j).B}, A={bitmap.GetPixel(i, j).A}");
+                            //    Console.WriteLine($"R={bitmap.GetPixel(i + 280, j).R}, " +
+                            //        $"G={bitmap.GetPixel(i + 280, j).G}, B={bitmap.GetPixel(i + 280, j).B}, A={bitmap.GetPixel(i + 280, j).A}");
 
-                        //}
-                        allcount++;
+                            //}
+                            allcount++;
+                        }
                     }
-                }
-                countOfPic++;
-                if ((double)count / allcount > 0.1)
-                {
-                    Console.WriteLine($"Pic# {countOfPic}");
-                    Console.WriteLine($"stepPic = {step}");
-                    //Console.WriteLine($"equal pixels = {count}");
-                    //Console.WriteLine($"all count = {allcount}");
-                    Console.WriteLine($"precision = {(double)count / allcount * 100}%");
-                    Console.WriteLine();
+                    countOfPic++;
+                    if ((double)count / allcount > 0.9)
+                    {
+                        Console.WriteLine($"Pic# {countOfPic}");
+                        Console.WriteLine($"stepPic = {stepX}");
+                        //Console.WriteLine($"equal pixels = {count}");
+                        //Console.WriteLine($"all count = {allcount}");
+                        Console.WriteLine($"precision = {(double)count / allcount * 100}%");
+                        Console.WriteLine();
+                    }
+
+                    count = 0; allcount = 0; stepX += stepPicX;
                 }
 
-                count = 0; allcount = 0; step += stepPic;
+                stepY += stepPicY;
             }
 
             Console.WriteLine($"work with {countOfPic} pics");
