c2a5cc6eb58f0b253c1d8d6e21215bf89ead37f8
SergioF <fs220381@gmail.com>
2019-08-18 16:52:38 +0300

    handling all array

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,54 +24,51 @@ namespace ConsoleApp1
             int count = 0;
             int allcount = 0;
             int countOfComparedPictures = 0;
-            int xCoordinateOfComparedPictures = 0;
-            int yCoordinateOfComparedPictures = 0;
-            int xCoordinateOfComparePictures = 0;
             int yCoordinateOfComparePictures = 0;
 
-            for (int xComparedPicture = 0; xComparedPicture < bitmap.Width; xComparedPicture += stepPicX)
+            for (int yComparePicture = 0; yComparePicture < bitmap.Height; yComparePicture += stepPicY)
             {
-                for (int yNextPictures = 0; yNextPictures < bitmap.Height; yNextPictures += stepPicY)
+                for (int xComparedPicture = 0; xComparedPicture < bitmap.Width; xComparedPicture += stepPicX)
                 {
-                    for (int xNextPictures = 0; xNextPictures < bitmap.Width; xNextPictures += stepPicX)
+                    for (int yNextPictures = 0; yNextPictures < bitmap.Height; yNextPictures += stepPicY)
                     {
-                        if ((xNextPictures == xComparedPicture && yNextPictures == 0) || 
-                            (xNextPictures<xComparedPicture && yNextPictures == 0)) continue;
-
-                        if (xComparedPicture < bitmap.Width)
+                        for (int xNextPictures = 0; xNextPictures < bitmap.Width; xNextPictures += stepPicX)
                         {
-                            for (int xCoordinate = 0; xCoordinate < stepPicX; xCoordinate++)
+                            if ((xNextPictures == xComparedPicture && yNextPictures == yComparePicture) ||
+                                (xNextPictures < xComparedPicture && yNextPictures < yComparePicture)) continue;
+
+                            if (xComparedPicture < bitmap.Width)
                             {
-                                for (int yCoordinate = 0; yCoordinate < 140; yCoordinate++)
+                                for (int xCoordinate = xComparedPicture; xCoordinate < xComparedPicture + stepPicX; xCoordinate++)
                                 {
-                                    if (bitmap.GetPixel(xCoordinate, yCoordinate).R == bitmap.GetPixel(xCoordinate + xNextPictures, yCoordinate + yNextPictures).R &&
-                                        bitmap.GetPixel(xCoordinate, yCoordinate).G == bitmap.GetPixel(xCoordinate + xNextPictures, yCoordinate + yNextPictures).G &&
-                                        bitmap.GetPixel(xCoordinate, yCoordinate).B == bitmap.GetPixel(xCoordinate + xNextPictures, yCoordinate + yNextPictures).B)
-                                        count++;
-                                    allcount++;
-                                    yCoordinateOfComparedPictures = yCoordinate + 1 - 140;
-                                    yCoordinateOfComparePictures = yCoordinate + 1 - 140;
+                                    for (int yCoordinate = yComparePicture; yCoordinate < yComparePicture + stepPicY; yCoordinate++)
+                                    {
+                                        if (bitmap.GetPixel(xCoordinate, yCoordinate).R == bitmap.GetPixel(xNextPictures + xCoordinate - xComparedPicture, yCoordinate + yNextPictures - yComparePicture).R &&
+                                            bitmap.GetPixel(xCoordinate, yCoordinate).G == bitmap.GetPixel(xNextPictures + xCoordinate - xComparedPicture, yCoordinate + yNextPictures - yComparePicture).G &&
+                                            bitmap.GetPixel(xCoordinate, yCoordinate).B == bitmap.GetPixel(xNextPictures + xCoordinate - xComparedPicture, yCoordinate + yNextPictures - yComparePicture).B)
+                                            count++;
+                                        allcount++;
+                                    }
                                 }
-                                xCoordinateOfComparedPictures = xCoordinate + xNextPictures - stepPicX + 1;
-                                xCoordinateOfComparePictures = xCoordinate + 1 - stepPicX;
+                                countOfComparedPictures++;
+
+                                if ((double)count / allcount > 0.2)
+                                {
+                                    Console.WriteLine($"X coordinate of compare Pic {xComparedPicture}");
+                                    Console.WriteLine($"Y coordinate of compare Pic {yComparePicture}");
+                                    Console.WriteLine($"X coordinate of compared Pic {xNextPictures}");
+                                    Console.WriteLine($"Y coordinate of compared Pic {yNextPictures}");
+                                    Console.WriteLine($"precision = {(double)count / allcount * 100}%");
+                                    Console.WriteLine($"xNext = {xNextPictures}");
+                                    Console.WriteLine();
+                                }
+
+                                count = 0; allcount = 0;
+
                             }
-                            countOfComparedPictures++;
-
-                            if ((double)count / allcount > 0.1)
-                            {
-                                Console.WriteLine($"X coordinate of compare Pic {xComparedPicture}");
-                                Console.WriteLine($"Y coordinate of compare Pic {yCoordinateOfComparePictures}");
-                                Console.WriteLine($"X coordinate of compared Pic {xNextPictures}");
-                                Console.WriteLine($"Y coordinate of compared Pic {yNextPictures}");
-                                Console.WriteLine($"precision = {(double)count / allcount * 100}%");
-                                Console.WriteLine($"xNext = {xNextPictures}");
-                                Console.WriteLine();
-                            }
-
-                            count = 0; allcount = 0;
-
                         }
                     }
+
                 }
 
             }
