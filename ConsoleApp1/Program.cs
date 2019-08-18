5842d2dd32333434bdfbce78fbd67659507ac3c1
SergioF <fs220381@gmail.com>
2019-08-18 20:52:15 +0300

    implemented paint method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string filePathName = @"D:\C#\test2rows5colum.png";
+            string filePathNameOutput = @"D:\C#\testColor.png";
 
             int stepPicX = 140;
             int stepPicY = 140;
@@ -24,7 +26,6 @@ namespace ConsoleApp1
             int count = 0;
             int allcount = 0;
             int countOfComparedPictures = 0;
-            int yCoordinateOfComparePictures = 0;
 
             for (int yComparePicture = 0; yComparePicture < bitmap.Height; yComparePicture += stepPicY)
             {
@@ -36,7 +37,7 @@ namespace ConsoleApp1
                         {
                             if ((xNextPictures == xComparedPicture && yNextPictures == yComparePicture) ||
                                 (xNextPictures < xComparedPicture && yNextPictures < yComparePicture) ||
-                                (xNextPictures < xComparedPicture && yNextPictures == yComparePicture) || 
+                                (xNextPictures < xComparedPicture && yNextPictures == yComparePicture) ||
                                 (yNextPictures < yComparePicture)) continue;
 
                             if (xComparedPicture < bitmap.Width)
@@ -54,7 +55,7 @@ namespace ConsoleApp1
                                 }
                                 countOfComparedPictures++;
 
-                                if ((double)count / allcount > 0.2)
+                                if ((double)count / allcount > 0.7)
                                 {
                                     Console.WriteLine($"X coordinate of compare Pic {xComparedPicture}");
                                     Console.WriteLine($"Y coordinate of compare Pic {yComparePicture}");
@@ -64,25 +65,47 @@ namespace ConsoleApp1
                                     Console.WriteLine($"xNext = {xNextPictures}");
                                     Console.WriteLine();
                                 }
-
                                 count = 0; allcount = 0;
-
                             }
                         }
                     }
-
                 }
-
             }
+
+            ColoredDublicate(bitmap, 280, 140, stepPicX);
+
+            if (File.Exists(filePathNameOutput))
+                File.Delete(filePathNameOutput);
+
+            bitmap.Save(filePathNameOutput);
 
             Console.WriteLine($"work with {countOfComparedPictures} pics");
             watch.Stop();
             var elapsedMs = watch.ElapsedMilliseconds;
             Console.WriteLine((double)elapsedMs / 1000);
+        }
 
+        private static void ColoredDublicate(Bitmap bitmap, int leftUpperCornerX, int leftUpperCornerY, int sideSizeRect)
+        {
+            Random randomColor = new Random();
 
+            Color colorBtm = Color.FromArgb(randomColor.Next(256), randomColor.Next(256), randomColor.Next(256));
+            for (int i = leftUpperCornerX; i < leftUpperCornerX + sideSizeRect; i++)
+            {
+                //Upper horizontal line
+                bitmap.SetPixel(i, leftUpperCornerY, colorBtm);
+                bitmap.SetPixel(i, leftUpperCornerY - 1, colorBtm);
+                //Right vertical line
+                bitmap.SetPixel(leftUpperCornerX + sideSizeRect - 1, i + leftUpperCornerY - leftUpperCornerX, colorBtm);
+                bitmap.SetPixel(leftUpperCornerX + sideSizeRect - 2, i + leftUpperCornerY - leftUpperCornerX, colorBtm);
+                //Bottom horizontal line
+                bitmap.SetPixel(i, leftUpperCornerY + sideSizeRect - 1, colorBtm);
+                bitmap.SetPixel(i, leftUpperCornerY + sideSizeRect - 2, colorBtm);
+                //Left vertical line
+                bitmap.SetPixel(leftUpperCornerX, i + leftUpperCornerY - leftUpperCornerX, colorBtm);
+                bitmap.SetPixel(leftUpperCornerX + 1, i + leftUpperCornerY - leftUpperCornerX, colorBtm);
 
-
+            }
 
         }
     }
