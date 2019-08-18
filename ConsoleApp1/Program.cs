717e7e7bf9ae00b91a70a451379b8e4bdba08b39
SergioF <fs220381@gmail.com>
2019-08-18 15:54:41 +0300

    all first row compare others pics

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -31,46 +31,47 @@ namespace ConsoleApp1
 
             for (int xComparedPicture = 0; xComparedPicture < bitmap.Width; xComparedPicture += stepPicX)
             {
-                //for (int yNextPictures = 0; yNextPictures < bitmap.Height; yNextPictures += stepPicY)
-                //{
-                for (int xNextPictures = xComparedPicture + stepPicX; xNextPictures < bitmap.Width; xNextPictures += stepPicX)
+                for (int yNextPictures = 0; yNextPictures < bitmap.Height; yNextPictures += stepPicY)
                 {
-                    if (xNextPictures == xComparedPicture) continue;
-
-                    if (xComparedPicture + stepPicX < bitmap.Width)
+                    for (int xNextPictures = 0; xNextPictures < bitmap.Width; xNextPictures += stepPicX)
                     {
-                        for (int xCoordinate = 0; xCoordinate < stepPicX; xCoordinate++)
+                        if ((xNextPictures == xComparedPicture && yNextPictures == 0) || 
+                            (xNextPictures<xComparedPicture && yNextPictures == 0)) continue;
+
+                        if (xComparedPicture < bitmap.Width)
                         {
-                            for (int yCoorfinate = 0; yCoorfinate < 140; yCoorfinate++)
+                            for (int xCoordinate = 0; xCoordinate < stepPicX; xCoordinate++)
                             {
-                                if (bitmap.GetPixel(xCoordinate, yCoorfinate).R == bitmap.GetPixel(xCoordinate + xNextPictures, yCoorfinate).R &&
-                                    bitmap.GetPixel(xCoordinate, yCoorfinate).G == bitmap.GetPixel(xCoordinate + xNextPictures, yCoorfinate).G &&
-                                    bitmap.GetPixel(xCoordinate, yCoorfinate).B == bitmap.GetPixel(xCoordinate + xNextPictures, yCoorfinate).B)
-                                    count++;
-                                allcount++;
-                                yCoordinateOfComparedPictures = yCoorfinate + 1 - 140;
-                                yCoordinateOfComparePictures = yCoorfinate + 1 - 140;
+                                for (int yCoordinate = 0; yCoordinate < 140; yCoordinate++)
+                                {
+                                    if (bitmap.GetPixel(xCoordinate, yCoordinate).R == bitmap.GetPixel(xCoordinate + xNextPictures, yCoordinate + yNextPictures).R &&
+                                        bitmap.GetPixel(xCoordinate, yCoordinate).G == bitmap.GetPixel(xCoordinate + xNextPictures, yCoordinate + yNextPictures).G &&
+                                        bitmap.GetPixel(xCoordinate, yCoordinate).B == bitmap.GetPixel(xCoordinate + xNextPictures, yCoordinate + yNextPictures).B)
+                                        count++;
+                                    allcount++;
+                                    yCoordinateOfComparedPictures = yCoordinate + 1 - 140;
+                                    yCoordinateOfComparePictures = yCoordinate + 1 - 140;
+                                }
+                                xCoordinateOfComparedPictures = xCoordinate + xNextPictures - stepPicX + 1;
+                                xCoordinateOfComparePictures = xCoordinate + 1 - stepPicX;
                             }
-                            xCoordinateOfComparedPictures = xCoordinate + xNextPictures - stepPicX + 1;
-                            xCoordinateOfComparePictures = xCoordinate + 1 - stepPicX;
+                            countOfComparedPictures++;
+
+                            if ((double)count / allcount > 0.1)
+                            {
+                                Console.WriteLine($"X coordinate of compare Pic {xComparedPicture}");
+                                Console.WriteLine($"Y coordinate of compare Pic {yCoordinateOfComparePictures}");
+                                Console.WriteLine($"X coordinate of compared Pic {xNextPictures}");
+                                Console.WriteLine($"Y coordinate of compared Pic {yNextPictures}");
+                                Console.WriteLine($"precision = {(double)count / allcount * 100}%");
+                                Console.WriteLine($"xNext = {xNextPictures}");
+                                Console.WriteLine();
+                            }
+
+                            count = 0; allcount = 0;
+
                         }
-                        countOfComparedPictures++;
-
-                        if ((double)count / allcount > 0.1)
-                        {
-                            Console.WriteLine($"X coordinate of compare Pic {xComparedPicture}");
-                            Console.WriteLine($"Y coordinate of compare Pic {yCoordinateOfComparePictures}");
-                            Console.WriteLine($"X coordinate of compared Pic {xNextPictures}");
-                            Console.WriteLine($"Y coordinate of compared Pic {yCoordinateOfComparedPictures}");
-                            Console.WriteLine($"precision = {(double)count / allcount * 100}%");
-                            Console.WriteLine($"xNext = {xNextPictures}");
-                            Console.WriteLine();
-                        }
-
-                        count = 0; allcount = 0;
-
                     }
-                    //}
                 }
 
             }
